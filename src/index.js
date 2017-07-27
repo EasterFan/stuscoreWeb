34cb365beb039f2cde429da84222a2656fb0ae05
EasterFan <fan.easter@gmail.com>
2017-07-27 10:59:12 +0800

    (function:)addStudenttoLocalStorage

--- src/index.js
+++ src/index.js
@@ -2,8 +2,9 @@
  * Created by easter on 17-7-25.
  */
 inputToLocalStoreage()
-var student = [];
 {
+    var student = [];
+
     var name = document.getElementsByName('name')[0].value;
     var sno = document.getElementsByName('sno')[0].value;
     var klass = document.getElementsByName('klass')[0].value;
@@ -13,5 +14,34 @@ var student = [];
 
     student.push(name,sno,klass,chinese,math,english);
     var str = JSON.stringify(student);
+    localStorage.obj = str;
+    localStorage.setItem('obj',str);
+}
 
+inputToLocalStoreage = function () {
+
+}
+
+
+function inputToLocalStoreage(){
+
+}
+
+
+let obj={
+    fun1:function () {
+
+    }
+};
+
+
+
+class Pe {
+    cons(){
+
+    }
+
+    fun(){
+
+    }
 }
