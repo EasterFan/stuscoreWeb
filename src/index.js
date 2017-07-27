d72f97fa685af618157b16cad3918d21150ce212
EasterFan <fan.easter@gmail.com>
2017-07-27 11:01:53 +0800

    (function:)addStudenttoLocalStorage

--- src/index.js
+++ src/index.js
@@ -1,47 +1,76 @@
 /**
  * Created by easter on 17-7-25.
  */
-inputToLocalStoreage()
-{
-    var student = [];
+'use strict'
 
-    var name = document.getElementsByName('name')[0].value;
-    var sno = document.getElementsByName('sno')[0].value;
-    var klass = document.getElementsByName('klass')[0].value;
-    var chinese = document.getElementsByName('chinese')[0].value;
-    var math = document.getElementsByName('math')[0].value;
-    var english = document.getElementsByName('english')[0].value;
+    // 先判断再初始化初始化---保留旧值，初始化一次只能存一条值
+    function init()
+    {
+        var allStudent;
 
-    student.push(name,sno,klass,chinese,math,english);
-    var str = JSON.stringify(student);
-    localStorage.obj = str;
-    localStorage.setItem('obj',str);
-}
+        if(localStorage.length == 0)
+        {
+            allStudent = [];
+        }else {
+            allStudent = JSON.parse(localStorage.allStudent);
+        }
 
-inputToLocalStoreage = function () {
-
-}
-
-
-function inputToLocalStoreage(){
-
-}
-
-
-let obj={
-    fun1:function () {
-
-    }
-};
-
-
-
-class Pe {
-    cons(){
+        return allStudent;
 
     }
 
-    fun(){
+    /* 学生信息添加到本地存储 */
+    function addStudentTolocalStorage()
+    {
+        var allStudent = init();
 
+
+        // event.preventDefault();  // 拦截submit表单提交动作，使提交内容留在本地console中
+
+        var student = {
+            sno: document.getElementsByName("sno")[0].value,
+            name: document.getElementsByName("name")[0].value,
+            nation: document.getElementsByName("nation")[0].value,
+            chinese: document.getElementsByName("chinese")[0].value,
+            math: document.getElementsByName("math")[0].value,
+            english: document.getElementsByName("english")[0].value,
+            prom: document.getElementsByName("prom")[0].value
+        };
+
+
+        allStudent.push(student);
+         // console.log(allStudent);
+        localStorage.allStudent = JSON.stringify(allStudent);
+
+        // localStorage.clear();
     }
-}
+
+
+    /* 查找学生信息 */
+
+// function searchStudent()
+// {
+//     var inputSno = document.getElementById("input_studentIds").value;
+//     //alert(inputSno);
+//     var allStudent = JSON.parse(localStorage.getItem("allStudent"));
+//     event.preventDefault();
+//     console.log(allStudent);
+//     for(let i=0;i<input_json_data.length;i++)
+//     {
+//         if(inputSno == input_json_data('sno')[i].id)
+//         {
+//             return `<tr>
+//                             <td>${input_json_data.name}</td>
+//                             <td>${input_json_data.math}</td>
+//                             <td>${input_json_data.chinese}</td>
+//                             <td>${input_json_data.english}</td>
+//                             <td>${input_json_data.prom}</td>
+//                             <td>${input_json_data.total}</td>
+//                             <td>${input_json_data.average}</td>
+//                         </tr>`
+//         }
+//
+//     }
+//
+// }
+
